f00b1583a40aacfd66aa4a431fe5cdb2e5a2e416
Tryanda Anggita Suwito <tryandaasu@gmail.com>
2024-11-04 19:56:33 +0700

    scripts untuk menampilkan alert

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+function showAlert() {
+    // Display the alert message
+    alert("Form submitted successfully!");
+    // Optionally, you can prevent the form from actually submitting if needed
+    return false; // Prevents default form submission
+}
